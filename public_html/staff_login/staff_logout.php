5fb09c77444c11415f64311c49c59b4430a91e1f
steiner9 <airtree_3@yahoo.co.jp>
2021-10-05 15:49:14 +0900

    10/5

---
+++ public_html/staff_login/staff_logout.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$_SESSION = array();
+if ($_COOKIE[session_name()] == true)
+{
+    setcookie(session_name(), '', time() - 420000, '/');
+}
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+ログアウトしました。<br />
+<br />
+<a href="../staff_login/staff_login.html">ログイン画面へ</a>
+
+</body>
+</html>
+
+
